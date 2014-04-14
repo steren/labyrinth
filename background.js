c2ddbf44947ed987fa1f6fa831b2e388b243968a
Steren <steren.giannini@gmail.com>
2014-04-14 10:54:03 +0200

    Create background.js for chrome app

---
+++ background.js
@@ -0,0 +1,10 @@
+/* global chrome */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    'bounds': {
+      'width': 600,
+      'height': 600
+    },
+    'resizable': true
+    });
+});
